c32c6346bd4ddbcaecc969827486fd0d4cb25f4d
Aitor Garcia <49533460+AitorGarciaM@users.noreply.github.com>
2019-11-14 22:21:37 +0100

    Add files via upload

---
+++ Menu.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+		<!-- Brand/logo -->
+			<a class="navbar-brand" href="#">Aitor <span class="bold">Garcia</span></a>
+			
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+		<!-- Links -->
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto" style="margin-left:78%">
+					<li class="nav-item">
+						<a class="nav-link" href="#section1">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#section2">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#section3">Projects</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#section4">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
