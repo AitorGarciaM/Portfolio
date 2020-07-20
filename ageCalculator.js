14bce4d4e432289d6eb422ea3c07a933c1cb878a
Aitor Garcia <49533460+AitorGarciaM@users.noreply.github.com>
2020-07-20 23:07:44 +0200

    Correcting a bug with age calculator

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,15 +1,34 @@
-function calculateAge(birthday)
+function calculateAge()
 {
+	var birthay = new Date(1997,6,29);
 	var today = new Date();
 
-	var age = today.getFullYear() - birthday.getFullYear();
-  
-    return Math.abs(age);	
-}
+	var miliseconds = Math.round(today-birthay);
+	var seconds = miliseconds / 1000;
+	var minutes = seconds / 60;
+	var hours = minutes / 60;
+	var days = hours / 24;
+	var years = days / 365.25;
 
-function writeAge()
-{
-	var td1 = document.getElementByID('#age');
-	var text = td1.document.createTextNode(calculateAge(new Date(1997,6,29)));
-	td1.appendChild(text);
+	var bMonth = birthay.getMonth()
+	var tMonth = today.getMonth()
+
+	var bDay = birthay.getDate();
+	var tDay = today.getDate();
+
+	if((bMonth - tMonth) > 0)
+	{
+		if((bDay - tDay) > 0)
+		{
+			return Math.floor(years);
+		}
+		else
+		{
+			return Math.ceil(years);
+		}
+	}
+	else
+	{
+		return Math.ceil(years);
+	}
 }
