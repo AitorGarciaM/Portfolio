c700ebb07b961ddc9cb0631980950f9880e55fb6
Aitor Garcia <49533460+AitorGarciaM@users.noreply.github.com>
2020-10-14 12:15:33 +0200

    Corrected calculateAge function

--- typewriterEffect.js
+++ typewriterEffect.js
@@ -60,36 +60,9 @@ var TxtType = function(el, toRotate, period) {
     };
 
     function calculateAge()
-{
-    var birthay = new Date(1997,7,21);
-    var today = new Date();
-
-    var miliseconds = Math.round(today-birthay);
-    var seconds = miliseconds / 1000;
-    var minutes = seconds / 60;
-    var hours = minutes / 60;
-    var days = hours / 24;
-    var years = days / 365.25;
-
-    var bMonth = birthay.getMonth()
-    var tMonth = today.getMonth()
-
-    var bDay = birthay.getDate();
-    var tDay = today.getDate();
-
-    if((bMonth - tMonth) > 0)
     {
-        if((bDay - tDay) > 0)
-        {
-            return Math.floor(years);
-        }
-        else
-        {
-            return Math.ceil(years);
-        }
-    }
-    else
-    {
-        return Math.ceil(years);
-    }
-}
+        var diff_ms = Date.now() - new Date(1997, 5, 29);
+	var age = new Date(diff_ms);
+	
+	return Math.abs(age.getUTCFullYear() - 1970);
+     }
