44cc3b37d4725b49429596d942da935b505e74d7
Aitor Garcia <jgaitor@msn.com>
2025-05-05 09:11:42 +0200

    Correcting links.

--- react-folder/src/components/Footer.jsx
+++ react-folder/src/components/Footer.jsx
@@ -15,19 +15,19 @@ export default function Footer() {
                 <p>&copy; {new Date().getFullYear()} My Portfolio</p>
                 <p>You can reach me at:</p>
                 <div className="social-links">
-                    <a>
+                    <a href="https://bsky.app/profile/aitordev.bsky.social">
                         <FontAwesomeIcon icon={faBluesky} size="xl" />
                     </a>
                     <a href="https://www.linkedin.com/in/aitor-garcia-mi%C3%B1ana/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faLinkedin} size="xl"/>
                     </a>
-                    <a href="https://www.linkedin.com/in/aitor-garcia-mi%C3%B1ana/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://aitor-garcia.itch.io/" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faItchIo} size="xl"/>
                     </a>
                     <a href="https://github.com/AitorGarciaM" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faGithub} size="xl"/>
                     </a>                    
-                    <a>
+                    <a href="mailto:aitor.garciamin@gmail.com">
                         <FontAwesomeIcon icon={faEnvelope} size="xl" />
                     </a>
                 </div>
