238a5e66acd4eea335e488629901eca5a4cdfcbe
Aitor Garcia <jgaitor@msn.com>
2025-05-05 09:54:06 +0200

    Corrected about me link on navbar.

--- react-folder/src/components/Navbar.jsx
+++ react-folder/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return(
         <section className="header">
             <section className="nav-bar">
-                <NavLink to="/Portfolio/about_me" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>ABOUT ME</NavLink>
+                <NavLink to="/Portfolio/" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>ABOUT ME</NavLink>
                 <NavLink to="/Portfolio/games" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>GAMES</NavLink>
                 <NavLink to="/Portfolio/resume" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>RESUME</NavLink>
                 <NavLink to="/Portfolio/contact" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>CONTACT</NavLink>
