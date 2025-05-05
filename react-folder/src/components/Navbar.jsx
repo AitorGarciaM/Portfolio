1bcb7d57c6089f02c4676e34af944c395f4dfcde
Aitor Garcia <jgaitor@msn.com>
2025-05-05 10:21:41 +0200

    Now route-link-active only applies if URL is exactly the same as 'to' parameter.

--- react-folder/src/components/Navbar.jsx
+++ react-folder/src/components/Navbar.jsx
@@ -4,10 +4,10 @@ export default function Navbar() {
     return(
         <section className="header">
             <section className="nav-bar">
-                <NavLink to="/Portfolio/" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>ABOUT ME</NavLink>
-                <NavLink to="/Portfolio/games" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>GAMES</NavLink>
-                <NavLink to="/Portfolio/resume" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>RESUME</NavLink>
-                <NavLink to="/Portfolio/contact" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")}>CONTACT</NavLink>
+                <NavLink to="/Portfolio/" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")} end>ABOUT ME</NavLink>
+                <NavLink to="/Portfolio/games" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")} end>GAMES</NavLink>
+                <NavLink to="/Portfolio/resume" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")} end>RESUME</NavLink>
+                <NavLink to="/Portfolio/contact" className={({ isActive }) => (isActive ? "route-link-active" : "nav-link")} end>CONTACT</NavLink>
             </section>
         </section>
     )
